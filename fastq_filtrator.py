21130d8b79dc320862d42615c0195bbdd41156aa
kupaeva <51483087+kupaeva@users.noreply.github.com>
2021-05-01 23:39:31 +0300

    Update fastq_filtrator.py

--- fastq_filtrator.py
+++ fastq_filtrator.py
@@ -1,16 +1,8 @@
 import sys
 
-# it is a block with default constant and flags
-min_length = 0
-keep_filtered = 0
-gc_bounds_low = None
-gc_bounds_high = 101
-output_base_name = 'file_name'
-four_lines = []
-i = 0
+list_arg = [i for i in sys.argv]
 
-# Usage
-Inf = "This tool can filter your fastq file.\n\
+inf = "This tool can filter your fastq file.\n\
 Standard call of tools looks like this:\n\
 python filter_fastq.py --some_argument some_values my_reads.fastq \n\n\
 Optional arguments:\n\
@@ -21,78 +13,109 @@ Optional arguments:\n\
     --output_base_name - common prefix name for all output files\n\
     --keep_filtered - flag for saving filtered reads."
 
-# block for argument parsing
-list_arg = [i for i in sys.argv]
-output_base_name = list_arg[-1]
-output_base_name = output_base_name.split('/')[-1].replace('.fastq', '')
-
-help_list = ['-h', '--help']
-possible_arguments = ['--min_length', '--gc_bounds', '--output_base_name', '--keep_filtered']
-
-if not set(help_list).isdisjoint(list_arg) or len(list_arg) <= 2:
-    print(Inf)
+if ('-h' in list_arg) or ('--help' in list_arg):
+    print(inf)
     sys.exit(0)
-for i in list_arg:
-    if "--" in i and i not in possible_arguments:
-        print("Wrong arguments!")
-        print(Inf)
 
-for i in range(1, len(list_arg)):
-    if i == len(list_arg) - 1:
-        if list_arg[i].endswith('.fastq'):
-            file = list_arg[i]
+
+def check_correctness(arg_list):
+    possible_arguments = ['--min_length',
+                          '--gc_bounds',
+                          '--output_base_name',
+                          '--keep_filtered']
+    if len(arg_list) <= 4:
+        raise ValueError('Not enough arguments! Run with -h or --help argument.')
+    for i in arg_list:
+        if "--" in i and i not in possible_arguments:
+            raise ValueError('Wrong arguments! Run with -h or --help argument.')
+    return True
+
+
+def parse_file_name(arg_list):
+    if arg_list[-1].endswith('.fastq'):
+        return arg_list[-1]
+    else:
+        raise ValueError('The last argument must be file in fastq format!')
+
+
+# must be int
+def parse_min_length(arg_list):
+    if '--min_length' in arg_list:
+        i = arg_list.index('--min_length')
+        if arg_list[i + 1].isnumeric():
+            return int(arg_list[i + 1])
         else:
-            print('The last argument must be file in fastq format!')
-            sys.exit(1)
-    elif list_arg[i] == '--min_length':
-        try:
-            int(list_arg[i + 1])
-        except ValueError:
-            print('Min_length value is not defined!')
-            sys.exit(1)
+            raise ValueError('Min_length value is set, but not defined!')
+    else:
+        return 0
+
+
+def parse_keep_filtered(arg_list):
+    if '--keep_filtered' in arg_list:
+        return 1
+    else:
+        return 0
+
+
+# must be int
+def parse_gc_bounds(arg_list):
+    if '--gc_bounds' in arg_list:
+        i = arg_list.index('--gc_bounds')
+        if arg_list[i + 1].isnumeric():
+            gc_low = int(arg_list[i + 1])
         else:
-            min_length = int(list_arg[i + 1])
-    elif list_arg[i] == '--keep_filtered':
-        keep_filtered = 1
-    elif list_arg[i] == '--gc_bounds':
+            raise ValueError('GC_bounds value is set, but not defined!')
         try:
-            int(list_arg[i + 1])
+            int(arg_list[i + 2])
         except ValueError:
-            print('GC_bounds value is not defined!')
-            sys.exit(1)
-        else:
-            gc_bounds_low = int(list_arg[i + 1])
-        try:
-            int(list_arg[i + 2])
-        except ValueError:
+            gc_high = 101
             pass
         else:
-            if int(list_arg[i + 2]) > gc_bounds_low:
-                gc_bounds_high = int(list_arg[i + 2])
+            if int(arg_list[i + 2]) > gc_low:
+                gc_high = int(arg_list[i + 2])
             else:
-                print('Wrong range of GC content!')
-                sys.exit(1)
-    elif list_arg[i] == '--output_base_name':
-        if list_arg[i + 1].endswith('.fastq') or list_arg[i + 1].startswith('--'):
-            print('Output base name is not specified!')
-            sys.exit(1)
+                raise ValueError('Wrong range of GC content!')
+        return gc_low, gc_high
+    else:
+        return None, 101
+
+
+def parse_output_base_name(arg_list):
+    if '--output_base_name' in arg_list:
+        i = arg_list.index('--output_base_name')
+        if arg_list[i + 1].endswith('.fastq') or arg_list[i + 1].startswith('--'):
+            raise ValueError('Output base name is set, but not specified!')
         else:
-            output_base_name = list_arg[i + 1]
-    elif list_arg[i] == '-h' or list_arg[i] == '--help':
-        print(Inf)
+            return arg_list[i + 1]
+    else:
+        name = arg_list[-1]
+        return name.split('/')[-1].replace('.fastq', '')
 
-# This part filter file according users setting
-out_failed = str(output_base_name + '__failed' + '.fastq')
-out_passed = str(output_base_name + '__passed' + '.fastq')
 
-with open(out_passed, 'w') as f:
-    pass
-if keep_filtered == 1:
-    with open(out_failed, 'w') as f:
+def genere_output_names(output_base_name):
+    out_failed = str(output_base_name + '__failed' + '.fastq')
+    out_passed = str(output_base_name + '__passed' + '.fastq')
+    return (out_failed, out_passed)
+
+
+def genere_files(out_passed, out_failed, keep_filtered):
+    with open(out_passed, 'w') as f:
         pass
+    if keep_filtered == 1:
+        with open(out_failed, 'w') as f:
+            pass
 
 
-def four_lines_read():
+# read lines
+def line_reader(four_lines_iterator):
+    try:
+        four_lines = next(four_lines_iterator)
+        return four_lines
+    except StopIteration:
+        return None
+
+
+def four_lines_read(file):
     with open(file, 'r') as f:
         res = []
         for line in f:
@@ -102,33 +125,62 @@ def four_lines_read():
                 res = []
 
 
-def filter_(four_lines_):
+# filtration
+
+def filter_(four_lines_, keep_filtered, out_failed):
     if keep_filtered == 1:
         with open(out_failed, 'a+') as f:
             for index_ in four_lines_:
                 f.write(index_)
 
 
-four_lines_iterator = four_lines_read()
-while True:
-    try:
-        four_lines = next(four_lines_iterator)
-    except StopIteration:
-        break
-    if len(four_lines[1][:-1]) < min_length:
-        if keep_filtered == 1:
-            filter_(four_lines)
-        continue
-    if gc_bounds_low is not None:
-        nucl_content = {'G': 0, 'C': 0, 'A': 0, 'T': 0}
-        for i in four_lines[1][:-1]:
-            nucl_content[i] = nucl_content.get(i, 0) + 1
-        GC_content = (nucl_content['C'] + nucl_content['G']) / len(four_lines[1][:-1])
-        if GC_content < gc_bounds_low / 100 or GC_content > gc_bounds_high / 100:
-            filter_(four_lines)
-            continue
+def count_gc(sequence):
+    g = sequence.count('G')
+    c = sequence.count('C')
+    if len(sequence) > 0:
+        return ((g + c) / len(sequence) * 100)
+    else:
+        return None
+
+
+def write_file(out_passed, four_lines):
     with open(out_passed, 'a+') as f:
         for index in four_lines:
             f.write(index)
 
-print('Process completed successfully')
+
+def filter_gc_content(four_lines, gc_bounds_low, gc_bounds_high):
+    gc_content = count_gc(four_lines[1][:-1])
+    if gc_content < gc_bounds_low or gc_content > gc_bounds_high:
+        return 0
+    return 1
+
+
+if __name__ == '__main__':
+    check = check_correctness(list_arg)
+    file = parse_file_name(list_arg)
+    min_length = parse_min_length(list_arg)
+    keep_filtered = parse_keep_filtered(list_arg)
+    gc_bounds_low, gc_bounds_high = parse_gc_bounds(list_arg)
+    output_base_name = parse_output_base_name(list_arg)
+    out_failed, out_passed = genere_output_names(output_base_name)
+    genere_files(out_passed, out_failed, keep_filtered)
+    four_lines_iterator = four_lines_read(file)
+    while True:
+        four_lines = line_reader(four_lines_iterator)
+        print(four_lines)
+        if four_lines is None:
+            break
+        if len(four_lines[1][:-1]) < min_length:
+            if keep_filtered == 1:
+                filter_(four_lines)
+            continue
+        if gc_bounds_low is not None:
+            if filter_gc_content(four_lines, gc_bounds_low, gc_bounds_high) == 1:
+                write_file(four_lines)
+            continue
+        else:
+            filter_(four_lines, keep_filtered, out_failed)
+            write_file(out_passed, four_lines)
+            continue
+    print('Process completed successfully')
